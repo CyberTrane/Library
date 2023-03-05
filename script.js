df73e10e5c3f75668340f169424f26d65456809d
CyberTrane <58540182+CyberTrane@users.noreply.github.com>
2023-03-05 03:22:16 -0500

    Completed display function, added some style

--- script.js
+++ script.js
@@ -18,26 +18,49 @@ function addBookToLibrary() {
     const author = prompt('Enter book author: ');
     const pages = prompt('Enter number of pages: ');
     const readCheck = prompt('Have you read this book? (y/n): ');
+    let read;
     if (readCheck === 'y') {
-        const read = true;
+        read = true;
     } else {
-        const read = false;
+        read = false;
     }
 
     myLibrary.push(new Book(title, author, pages, read));
 }
 
 function displayBooks() {
+    const main = document.querySelector('.main');
     for (let i = 0; i < myLibrary.length; i++) {
-        const card = document.createElement('div').classList.add('card');
-        const title = document.createElement('h3').classList.add('title');
-        const author = document.createElement('p').classList.add('author');
-        const pages = document.createElement('p').classList.add('pages');
-        const read = document.createElement('p').classList.add('read');
-        
+        const card = document.createElement('div');
+        card.classList.add('card');
+
+        const title = document.createElement('h3');
+        title.classList.add('title');
+        title.innerText = myLibrary[i].title;
+
+        const author = document.createElement('p');
+        author.classList.add('author');
+        author.innerText = myLibrary[i].author;
+
+        const pages = document.createElement('p');
+        pages.classList.add('pages');
+        pages.innerText = myLibrary[i].pages;
+
+        const read = document.createElement('p');
+        read.classList.add('read');
+        read.innerText = myLibrary[i].read;
+
+        card.appendChild(title);
+        card.appendChild(author);
+        card.appendChild(pages);
+        card.appendChild(read);
+
+        main.appendChild(card);
     }
 }
 
 let myLibrary = [];
 myLibrary.push(new Book('Beyond Good and Evil', 'Friedrich Nietzsche', 217, true));
 myLibrary.push(new Book('Animal Farm', 'George Orwell', 392, false));
+
+displayBooks();
