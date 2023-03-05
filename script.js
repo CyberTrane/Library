c86d1ed47efe09990f0a02a5a7b0ee9d644f7426
CyberTrane <58540182+CyberTrane@users.noreply.github.com>
2023-03-05 16:12:10 -0500

    Added button for new entries,basic style for cards

--- script.js
+++ script.js
@@ -1,66 +1,70 @@
 function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    if (read) {
-        this.read = 'read';
-    } else {
-        this.read = 'not read yet';
-    }
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  if (read) {
+    this.read = "read";
+  } else {
+    this.read = "not read yet";
+  }
 }
 
-Book.prototype.info = function() {
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
-}
+Book.prototype.info = function () {
+  return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
+};
 
 function addBookToLibrary() {
-    const title = prompt('Enter book title: ');
-    const author = prompt('Enter book author: ');
-    const pages = prompt('Enter number of pages: ');
-    const readCheck = prompt('Have you read this book? (y/n): ');
-    let read;
-    if (readCheck === 'y') {
-        read = true;
-    } else {
-        read = false;
-    }
+  const title = prompt("Enter book title: ");
+  const author = prompt("Enter book author: ");
+  const pages = prompt("Enter number of pages: ");
+  const readCheck = prompt("Have you read this book? (y/n): ");
+  let read;
+  if (readCheck === "y") {
+    read = true;
+  } else {
+    read = false;
+  }
 
-    myLibrary.push(new Book(title, author, pages, read));
+  myLibrary.push(new Book(title, author, pages, read));
 }
 
 function displayBooks() {
-    const main = document.querySelector('.main');
-    for (let i = 0; i < myLibrary.length; i++) {
-        const card = document.createElement('div');
-        card.classList.add('card');
+  const main = document.querySelector(".main");
+  for (let i = 0; i < myLibrary.length; i++) {
+    const card = document.createElement("div");
+    card.classList.add("card");
 
-        const title = document.createElement('h3');
-        title.classList.add('title');
-        title.innerText = myLibrary[i].title;
+    const title = document.createElement("h3");
+    title.classList.add("title");
+    title.innerText = myLibrary[i].title;
 
-        const author = document.createElement('p');
-        author.classList.add('author');
-        author.innerText = myLibrary[i].author;
+    const author = document.createElement("p");
+    author.classList.add("author");
+    author.innerText = "by " + myLibrary[i].author;
 
-        const pages = document.createElement('p');
-        pages.classList.add('pages');
-        pages.innerText = myLibrary[i].pages;
+    const pages = document.createElement("p");
+    pages.classList.add("pages");
+    pages.innerText = myLibrary[i].pages + " pages";
 
-        const read = document.createElement('p');
-        read.classList.add('read');
-        read.innerText = myLibrary[i].read;
+    const read = document.createElement("p");
+    read.classList.add("read");
+    read.innerText = myLibrary[i].read;
 
-        card.appendChild(title);
-        card.appendChild(author);
-        card.appendChild(pages);
-        card.appendChild(read);
+    card.appendChild(title);
+    card.appendChild(author);
+    card.appendChild(pages);
+    card.appendChild(read);
 
-        main.appendChild(card);
-    }
+    main.appendChild(card);
+  }
 }
 
 let myLibrary = [];
-myLibrary.push(new Book('Beyond Good and Evil', 'Friedrich Nietzsche', 217, true));
-myLibrary.push(new Book('Animal Farm', 'George Orwell', 392, false));
+myLibrary.push(
+  new Book("Beyond Good and Evil", "Friedrich Nietzsche", 217, true)
+);
+myLibrary.push(new Book("Animal Farm", "George Orwell", 392, false));
 
 displayBooks();
+
+const newBookBtn = document.querySelector(".newBook");
